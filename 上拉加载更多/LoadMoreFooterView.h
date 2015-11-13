a55708a3dc3e381564ea94892fb19461106da089
chenhao <chenh@kunion.com.cn>
2015-11-13 10:41:18 +0800

    Initial commit

---
+++ 上拉加载更多/LoadMoreFooterView.h
@@ -0,0 +1,13 @@
+//
+//  LoadMoreFooterView.h
+//  上拉加载更多
+//
+//  Created by jhtxch on 15/11/11.
+//  Copyright © 2015年 jhtxch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadMoreFooterView : UIView
+
+@end
---
+++ 上拉加载更多/LoadMoreFooterView.m
@@ -0,0 +1,21 @@
+//
+//  LoadMoreFooterView.m
+//  上拉加载更多
+//
+//  Created by jhtxch on 15/11/11.
+//  Copyright © 2015年 jhtxch. All rights reserved.
+//
+
+#import "LoadMoreFooterView.h"
+
+@implementation LoadMoreFooterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
